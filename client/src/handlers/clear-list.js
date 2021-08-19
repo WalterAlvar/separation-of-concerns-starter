b22dd760592f8f8e884efbb771ffc15679f6e6a5
deepa@hyf123 <deepamlavil@gmail.com>
2021-08-19 20:29:12 +0200

    modified clearlist

--- client/src/handlers/clear-list.js
+++ client/src/handlers/clear-list.js
@@ -2,8 +2,10 @@ const todoList = document.querySelector('.todo-list');
 
 export const clearList = (event) => {
     event.preventDefault();
-    confirm('Are you sure you want to clear the entire list?');
-    while (todoList.lastChild) {
-        todoList.removeChild(todoList.lastChild);
+    const userConfirm = confirm('Are you sure you want to clear the entire list?');
+    if(userConfirm){
+        while (todoList.lastChild) {
+            todoList.removeChild(todoList.lastChild);
+        }
     }
 }
