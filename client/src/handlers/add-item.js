af247e00bd1bf5334edd0ab9cc27200f072d5a00
deepa-thomas <78364046+deepa-thomas@users.noreply.github.com>
2021-08-24 18:39:13 +0200

    updated syntax issues
    updated syntax issues

--- client/src/handlers/add-item.js
+++ client/src/handlers/add-item.js
@@ -4,7 +4,7 @@ const todoList = document.querySelector('.todo-list');
 export const addItem = (event) => {
   event.preventDefault();
   const newToDoName = todoInput.value;
-  if (newToDoName != '') {
+  if (newToDoName !== '') {
     if (newToDoName.length > 80) {
       alert('Length should be less than 80 characters');
     } else {
@@ -12,11 +12,11 @@ export const addItem = (event) => {
       const newToDoItem = document.createElement('li');
       const checkBox = document.createElement('input');
       checkBox.type = 'checkbox';
-      checkBox.id = 'chk-' + newToDoName;
+      checkBox.id = `chk-${newToDoName}`;
       checkBox.checked = false;
       newToDoItem.appendChild(checkBox); // Append the checkbox to the <li>
-      const newItem = document.createTextNode(newToDoName); // Create the text node after the the checkbox
-      newToDoItem.appendChild(newItem); // Append the text node to the <li>
+      const newItemText = document.createTextNode(newToDoName); // Create the text node after the checkbox
+      newToDoItem.appendChild(newItemText); // Append the text node to the <li>
       todoDiv.append(newToDoItem);
       todoList.append(newToDoItem);
 
