5001520ed50b7d7f814b3d9a44a3025c22da8671
Walter Alvarado <yovaemperor@gmail.com>
2021-08-26 12:02:59 +0200

    testing JS

--- client/src/init/index.js
+++ client/src/init/index.js
@@ -7,3 +7,29 @@ import '../handlers/remove-item.js';
 import '../listeners/remove-button-listener.js';
 import '../listeners/nightmode-listener.js';
 
+//sort list
+
+
+const sortBtn = document.querySelector('.sorting-todo');
+sortBtn.addEventListener('click', sortList);
+
+const todoList = document.querySelector('.todo-list');
+const param = document.querySelectorAll('.liToSort');
+
+const sortList = (event) => {
+    event.preventDefault();
+    const sortedList = todoList
+  };  
+
+
+/*
+  const sortWords = (event) => {
+    const howToSort = event.target.value;//gather user input from DOM
+    const sorted = sortStrings(data.words, howToSort);
+    //Render new list
+    const newList = renderList(sorted);
+    const listContainer = document.getElementById('list-container');
+  listContainer.innerHTML = '';
+  listContainer.appendChild(newList);
+};
+*/
